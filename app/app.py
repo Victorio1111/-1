2eded419ea4f0504f0a40490cf3bf0105f3c98a6
Victor <viktorio2220@gmail.com>
2025-04-24 19:18:41 +0300

    Html структура главной страницы

--- app/app.py
+++ app/app.py
@@ -1,12 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__) # директива __name__ указывает на имя файла
+
+# Обработчик, Декоратор
+@app.route("/index")  
+@app.route("/")  
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True) # при debug = True сервер будет автоматически перезапускаться + обработка ошибок
   
-# Обработчик
-@app.rout("/")  
-def index():
-    return "index"
+
   
