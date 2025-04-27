477c3c910726b949becfa31afdba2e97ec6250ef
Victor <viktorio2220@gmail.com>
2025-04-28 01:41:23 +0300

    Настроил выпадающий список с функцией поиска

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -1,28 +1,16 @@
-// Ждем полной загрузки HTML-страницы
 document.addEventListener("DOMContentLoaded", function() {
-
-    // Вызываем функцию для загрузки компаний в выпадающий список
     fetch("/get_companies")
-        .then(response => response.json()) // Преобразуем ответ сервера в формат JSON
-        .then(data => {
-            // Находим элемент на странице по его id
-            const companySelect = document.getElementById("company-select");
+    .then(response => response.json())
+    .then(data => {
+        const datalist = document.getElementById("companies");
 
-            // Проходимся по каждой компании в полученных данных
-            data.forEach(company => {
-                // Создаем новый элемент <option> для выпадающего списка
-                const option = document.createElement("option");
-
-                // Значение и текст внутри опции будет названием компании
-                option.value = company.name;
-                option.textContent = company.name;
-
-                // Добавляем опцию в выпадающий список
-                companySelect.appendChild(option);
-            });
-        })
-        .catch(error => {
-            // Если при загрузке данных произошла ошибка — выводим её в консоль
-            console.error('Ошибка при загрузке компаний:', error);
+        data.forEach(company => {
+            const option = document.createElement("option");
+            option.value = company.name;
+            datalist.appendChild(option);
         });
+    })
+    .catch(error => {
+        console.error('Ошибка при загрузке компаний:', error);
+    });
 });
